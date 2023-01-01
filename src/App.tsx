d7a13fde2bb7abefdd6bad429d9570898a04a2a9
Francis Daniel Valmores <francisdaniel.valmores@engagis.com>
2023-01-02 00:15:07 +0800

    installed bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div>
       <PrimaryButton text="Hello, world!" />
+      <div className="d-flex justify-content-center mt-5">
+        <h1>Hello, world!</h1>
+      </div>
     </div>
   );
 }
