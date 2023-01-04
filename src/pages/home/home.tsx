06d257aee4ba9949ff16def151036e04db6c1b1d
Francis Daniel Valmores <francisdaniel.valmores@engagis.com>
2023-01-04 14:21:27 +0800

    footer

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,6 +2,7 @@ import "./home.css";
 
 import TopCover from "./components/top-cover/top-cover";
 import Profile from "./components/profile/profile";
+import Footer from "./components/footer/footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,9 @@ export default function Home() {
       </div>
       <div className="body">
         <Profile />
+      </div>
+      <div className="footer">
+        <Footer />
       </div>
     </div>
   );
